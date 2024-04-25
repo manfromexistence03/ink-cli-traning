740967f65fad623ab5b2d23976382e2227dadee9
manfromexistence03 Emon <manfromexistence03@gmail.com>
2024-04-25 13:53:59 +0000

    feat: how to hide cursor in ink.

---
+++ cli/source/example.tsx
@@ -0,0 +1,42 @@
+import React
+    from 'react';
+import { render, Text, type TextProps } from 'ink';
+import {
+    ThemeProvider,
+    defaultTheme,
+    extendTheme,
+    useComponentTheme,
+    type ComponentTheme,
+} from '@inkjs/ui';
+
+const customLabelTheme = {
+    styles: {
+        label: (): TextProps => ({
+            color: 'green',
+        }),
+    },
+} satisfies ComponentTheme;
+
+type CustomLabelTheme = typeof customLabelTheme;
+
+const customTheme = extendTheme(defaultTheme, {
+    components: {
+        CustomLabel: customLabelTheme,
+    },
+});
+
+function CustomLabel() {
+    const { styles } = useComponentTheme<CustomLabelTheme>('CustomLabel');
+
+    return <Text {...styles.label()}>Hello world</Text>;
+}
+
+function Example() {
+    return (
+        <ThemeProvider theme={customTheme}>
+            <CustomLabel />
+        </ThemeProvider>
+    );
+}
+
+render(<Example />);
